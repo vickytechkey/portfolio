2f644ea6e63b5ce540db27d4f5fc0a1ce00545c8
vickytechkey <vichunice@gmail.com>
2021-07-29 15:35:35 +0530

    animation nav added

--- src/webcomponents/navbar.js
+++ src/webcomponents/navbar.js
@@ -1,9 +1,25 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 function NavigationBar() {
+    const routes = [
+        { 'path': "/", 'label': 'Home' },
+        { 'path': "/projects", 'label': 'My works' },
+        { 'path': "/about", 'label': 'About myself' },
+        { 'path': "/contact", 'label': 'Say Hello' },
+        
+    
+    ]
     return (
         <React.Fragment>
-            <h1>NavigationBar</h1>
+            <nav className="z-depth-0" >
+    <div className="nav-wrapper white z-depth-0 " >
+      <a href="#" class="brand-logo">Logo</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        {routes.map((element, index) => <li key={index}><Link className="black-text hvr-underline-from-right" style={{ fontSize: 25, fontFamily: ['Poppins', 'sans-serif'] }} to={element.path}>{ element.label}</Link></li>)}
+                       
+                    </ul>
+    </div>
+  </nav>
         </React.Fragment>
     );
 }
