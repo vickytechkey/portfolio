58b06c72cc052fe3cccbecc1aebdfa0e8206fc2b
vickytechkey <vichunice@gmail.com>
2021-07-30 13:21:18 +0530

    firebase added

--- src/pages/home/projects.js
+++ src/pages/home/projects.js
@@ -1,6 +1,20 @@
 import React from 'react';
-
+import firebase from "firebase/app";
+import "firebase/firestore";
 function Projects() {
+    const firebaseConfig = {
+        apiKey: "AIzaSyCbNRhYBErmJq_Yxl7uaV8Wd3ViGJaYcaM",
+        authDomain: "vigneshexplorations.firebaseapp.com",
+        projectId: "vigneshexplorations",
+        storageBucket: "vigneshexplorations.appspot.com",
+        messagingSenderId: "1061942842544",
+        appId: "1:1061942842544:web:cc0a41c1a4a58e9abb9775",
+        measurementId: "G-1JG82313DL"
+    };
+   
+    if (!firebase.default.initializeApp.length) {
+        firebase.initializeApp(firebaseConfig);
+    }
     return (
         <React.Fragment>
          
