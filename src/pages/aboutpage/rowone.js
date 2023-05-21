ae00855dd115f03faff6f946e3033f50153f35f7
vignesh <vichunice@gmail.com>
2023-05-21 15:54:57 +0530

    changing  font size

--- src/pages/aboutpage/rowone.js
+++ src/pages/aboutpage/rowone.js
@@ -1,21 +1,37 @@
-import React from 'react';
-import Asset5 from '../../assets/about/asset5.png'
+import React from "react";
+import Asset5 from "../../assets/about/asset5.png";
 function Rowone() {
-    return (
-        <React.Fragment>
-           <div className="row">
-              
-                <div className="col xl6 l6 s12 m12">
-                   <img src={Asset5} alt={Asset5} className="responsive-img" />
-                </div>
-                <div className="col xl6 l6 s12 m12">
-                    <h4 data-aos="zoom-in-left" style={{fontSize : 20 , lineHeight : 1.6 , fontFamily: ['Poppins', 'sans-serif']}}>
-                    I discovered my passion for coding when I was a student in High School; It was then when it became clear what I wanted to do for a living. I pursued my career at the sree sakthi engineering College where I started my journey towards my career. A month later I took a begginer’s Web className, where I discovered the love for code. After that, I switched my major to Interactive Design and started the never-ending journey of becoming a web developer along with sharpening my skills for coding. In 2015 completed by php course and start my journey as php coder from college app developer club and help the developers to create the server function and turn my focus toward the android app development
-                   </h4>
-                   </div>
-           </div>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <div className="row">
+        <div className="col xl6 l6 s12 m12">
+          <img src={Asset5} alt={Asset5} className="responsive-img" />
+        </div>
+        <div className="col xl6 l6 s12 m12">
+          <h4
+            data-aos="zoom-in-left"
+            style={{
+              fontSize: 20,
+              lineHeight: 1.6,
+              fontFamily: ["Poppins", "sans-serif"],
+            }}
+          >
+            I discovered my passion for coding when I was a student in High
+            School; It was then when it became clear what I wanted to do for a
+            living. I pursued my career at the sree sakthi engineering College
+            where I started my journey towards my career. A month later I took a
+            begginer’s Web className, where I discovered the love for code.
+            After that, I switched my major to Interactive Design and started
+            the never-ending journey of becoming a web developer along with
+            sharpening my skills for coding. In 2015 completed by php course and
+            start my journey as php coder from college app developer club and
+            help the developers to create the server function and turn my focus
+            toward the android app development
+          </h4>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default Rowone;
--- src/pages/aboutpage/rowthree.js
+++ src/pages/aboutpage/rowthree.js
@@ -1,55 +1,71 @@
-import React from 'react';
-import { PurpleBarC } from '../home/purplebar';
-import WorkImage from '../../assets/about/work.jpg'
+import React from "react";
+import { PurpleBarC } from "../home/purplebar";
+import WorkImage from "../../assets/about/work.jpg";
 function RowThree() {
-    return (
-        <React.Fragment>
-           <div className="row" data-aos="zoom-in-up">
-               <div className="col offset-xl8 offset-l8 s1 m1 xl1 l1">
-                   <PurpleBarC height="50px" />
-                </div>
-                <div className="col s4 m4 xl3 l3 hide-on-med-and-down">
-                <h1 style={{ marginTop : -55,marginLeft : -55 , fontSize : 45 , lineHeight : 3.6 , fontFamily: ['Poppins', 'sans-serif']}}>Work Experience</h1>
-                </div>
-                <div className="col s4 m4 xl3 l3 hide-on-med-and-up">
-                <h1 style={{ marginTop : -55,marginLeft : -15 , fontSize : 45 , lineHeight : 3.6 , fontFamily: ['Poppins', 'sans-serif']}}>Work Experience</h1>
-                </div>
+  return (
+    <React.Fragment>
+      <div className="row" data-aos="zoom-in-up">
+        <div className="col offset-xl8 offset-l8 s1 m1 xl1 l1">
+          <PurpleBarC height="50px" />
+        </div>
+        <div className="col s4 m4 xl3 l3 hide-on-med-and-down">
+          <h1
+            style={{
+              marginTop: -55,
+              marginLeft: -55,
+              fontSize: 45,
+              lineHeight: 3.6,
+              fontFamily: ["Poppins", "sans-serif"],
+            }}
+          >
+            Work Experience
+          </h1>
+        </div>
+        <div className="col s4 m4 xl3 l3 hide-on-med-and-up">
+          <h1
+            style={{
+              marginTop: -55,
+              marginLeft: -15,
+              fontSize: 45,
+              lineHeight: 3.6,
+              fontFamily: ["Poppins", "sans-serif"],
+            }}
+          >
+            Work Experience
+          </h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col xl6 l6 s12 m12 " style={{ marginTop: 75 }}>
+          <div className="row" data-aos="zoom-in-up" data-aos-delay="300">
+            <div className="col xl1 l1 s12 m12">
+              <i
+                className="material-icons Large deep-purple-text darken-4"
+                style={{ fontSize: 40 }}
+              >
+                work
+              </i>
             </div>
-            <div className="row">
-                <div className="col xl6 l6 s12 m12 " style={{marginTop:75}}>
-                <div className="row" data-aos="zoom-in-up" data-aos-delay="300">
-                        <div className="col xl1 l1 s12 m12">
-                        <i className="material-icons Large deep-purple-text darken-4" style={{fontSize:40}}>work</i>
-                        </div>
-                        <div className="col xl9 l9 s12 m12" style={{marginTop:-30}}>
-                        <h5 style={{fontSize : 20 , lineHeight : 3.6 , fontFamily: ['Poppins', 'sans-serif']}}>Amazon software support engineer- 2020 to present</h5>
-                        </div>
-                    </div>
-                    {/* ends */}
-                {/* <div className="row" data-aos="zoom-in-up" data-aos-delay="300">
-                        <div className="col xl1 l1 s12 m12">
-                        <i className="material-icons Large deep-purple-text darken-4" style={{fontSize:40}}>work</i>
-                        </div>
-                        <div className="col xl6 l6 s12 m12" style={{marginTop:-30}}>
-                        <h5 style={{fontSize : 20 , lineHeight : 3.6 , fontFamily: ['Poppins', 'sans-serif']}}>Amazon Resolution Specalist - 2020 </h5>
-                        </div>
-                    </div> */}
-                    {/* ends */}
-                     {/* <div className="row" data-aos="zoom-in-up" data-aos-delay="600">
-                        <div className="col xl1 l1 s12 m12">
-                        <i className="material-icons Large  deep-purple-text darken-4" style={{fontSize:40}}>work</i>
-                        </div>
-                        <div className="col xl6 l6 s12 m12" style={{marginTop:-30}}>
-                        <h5 style={{fontSize : 20 , lineHeight : 3.6 , fontFamily: ['Poppins', 'sans-serif']}}>Amazon CSA - 2018</h5>
-                        </div>
-                       </div> */}
-                </div> 
-                <div className="col xl6 l6 s12 m12 ">
-                <img src={WorkImage}  alt={WorkImage}   className="responsive-img" />
-                    </div>
+            <div className="col xl9 l9 s12 m12" style={{ marginTop: -30 }}>
+              <h5
+                style={{
+                  fontSize: 20,
+                  lineHeight: 3.6,
+                  fontFamily: ["Poppins", "sans-serif"],
+                }}
+              >
+                Amazon software support engineer- 2020 to present
+              </h5>
             </div>
-        </React.Fragment>
-    );
+          </div>
+          {/* ends */}
+        </div>
+        <div className="col xl6 l6 s12 m12 ">
+          <img src={WorkImage} alt={WorkImage} className="responsive-img" />
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default RowThree;
