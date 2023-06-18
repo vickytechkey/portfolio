4e2f4b745e3fe2a1e06316c4fecf53142cda2383
vickytechkey <vichunice@gmail.com>
2023-06-18 12:18:03 +0530

    upgraded react to 18.2.0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,26 +7,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { initializeApp } from "firebase/app";
 import AppConfiguration from './configuration/mainconfiguration';
 import 'hover.css/css/hover.css';
-// import { Provider } from 'react-redux'
-// import store from './store'
 
 const appconfig = new AppConfiguration();
 const app = initializeApp(appconfig.firebaseHostConfig);
-
-ReactDOM.render(
-
-  <BrowserRouter>
-
-    <App />
-   
-  </BrowserRouter>
-
-  
-  ,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<BrowserRouter><App/></BrowserRouter>);
+ // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
