9851ce260f0d58546eb5fbd2530b6ab045e698e8
vignesh k <krvigne@amazon.com>
2024-04-26 08:53:10 +0530

    adding test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,22 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from 'react-router-dom';
+
 import React from "react";
 import HomePage from "./pages/homepage";
 import AboutPage from "./pages/aboutpage";
 import ContactPage from "./pages/contactpage";
 import ProjectPage from "./pages/projectspage";
 test("renders main pages", () => {
-  render(<HomePage url="/" />);
-  render(<AboutPage url="/about" />);
-  render(<ContactPage url="/contact" />);
-  render(<ProjectPage url="/projects" />);
+  render(<HomePage /> , {wrapper: MemoryRouter});
 });
+test("render about page" , ()=>{
+  render(<AboutPage /> , {wrapper: MemoryRouter});
+})
+
+test("render contact page" , ()=>{
+  render(<ContactPage /> , {wrapper: MemoryRouter});
+})
+
+test("render contact page" , ()=>{
+  render(<ProjectPage /> , {wrapper: MemoryRouter});
+})
