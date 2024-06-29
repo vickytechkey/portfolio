91aace30893e1be3357fa98b7ed7fde636ddcb44
vignesh k <krvigne@amazon.com>
2024-06-29 07:05:39 +0530

    adding scikitlearn

--- src/configuration/skills.js
+++ src/configuration/skills.js
@@ -6,17 +6,30 @@ import Angular from "../assets/homepage/skills/angular.svg";
 import Python from "../assets/homepage/skills/python.svg";
 import ReactJs from "../assets/homepage/skills/reactjs.svg";
 import Spark from "../assets/homepage/skills/spark.svg";
+import Scikit from "../assets/homepage/skills/scikitlearn.svg";
+// import pytorch from 
+// import seaborn from
+// import pandas from
+// import etl from
+// import apacheairflow from
+// import pyspark from
+// import rag from
+// import genai from
 export default function skillsarray() {
   const logos = [
     "br",
+    Scikit,
     Angular,
     Java,
     Flutter,
     NodeJs,
     ReactJs,
-    Python,
     "br",
+    Python,
     Spark,
   ];
   return logos;
 }
+
+// color code #6600FF
+//size 1024 X 1024
