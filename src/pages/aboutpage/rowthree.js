891a8973f181dfabc2cbcb775136f9a23e1d864c
vignesh K <31525460+vickytechkey@users.noreply.github.com>
2025-09-06 10:53:10 +0530

    updating jobs in about page

--- src/pages/aboutpage/rowthree.js
+++ src/pages/aboutpage/rowthree.js
@@ -1,91 +1,60 @@
 import React from "react";
 import { PurpleBarC } from "../home/purplebar";
 import WorkImage from "../../assets/about/work.jpg";
-function RowThree() {
+
+function WorkItem({ title, delay }) {
   return (
-    <React.Fragment>
+    <div className="row" data-aos="zoom-in-up" data-aos-delay={delay}>
+      <div className="col xl1 l1 s12 m12 flex items-center justify-center">
+        <i className="material-icons Large deep-purple-text darken-4 text-4xl">
+          work
+        </i>
+      </div>
+      <div className="col xl9 l9 s12 m12 -mt-6">
+        <h5 className="text-xl leading-[3.6rem] font-[Poppins]">{title}</h5>
+      </div>
+    </div>
+  );
+}
+
+function RowThree() {
+  const workExperiences = [
+    "LTIMindtree Data Engineer (2025 - Present)", // You can replace with other jobs
+    "Amazon Application Engineer-2 (2018 - 2025)", // You can replace with other jobs
+  ];
+
+  return (
+    <section>
+      {/* Header Row */}
       <div className="row" data-aos="zoom-in-up">
         <div className="col offset-xl8 offset-l8 s1 m1 xl1 l1">
           <PurpleBarC height="50px" />
         </div>
-        <div className="col s4 m4 xl3 l3 hide-on-med-and-down">
-          <h1
-            style={{
-              marginTop: -55,
-              marginLeft: -55,
-              fontSize: 45,
-              lineHeight: 3.6,
-              fontFamily: ["Poppins", "sans-serif"],
-            }}
-          >
+        <div className="col s4 m4 xl3 l3">
+          <h2 className="text-4xl font-[Poppins] -mt-14 -ml-14 leading-[3.6rem] hide-on-med-and-down">
             Work Experience
-          </h1>
-        </div>
-        <div className="col s4 m4 xl3 l3 hide-on-med-and-up">
-          <h1
-            style={{
-              marginTop: -55,
-              marginLeft: -15,
-              fontSize: 45,
-              lineHeight: 3.6,
-              fontFamily: ["Poppins", "sans-serif"],
-            }}
-          >
+          </h2>
+          <h2 className="text-4xl font-[Poppins] -mt-14 -ml-4 leading-[3.6rem] hide-on-med-and-up">
             Work Experience
-          </h1>
+          </h2>
         </div>
       </div>
+
+      {/* Content Row */}
       <div className="row">
-        <div className="col xl6 l6 s12 m12 " style={{ marginTop: 75 }}>
-          <div className="row" data-aos="zoom-in-up" data-aos-delay="300">
-            <div className="col xl1 l1 s12 m12">
-              <i
-                className="material-icons Large deep-purple-text darken-4"
-                style={{ fontSize: 40 }}
-              >
-                work
-              </i>
-            </div>
-            <div className="col xl9 l9 s12 m12" style={{ marginTop: -30 }}>
-              <h5
-                style={{
-                  fontSize: 20,
-                  lineHeight: 3.6,
-                  fontFamily: ["Poppins", "sans-serif"],
-                }}
-              >
-                Amazon Application Engineer- I 2021 to 2022
-              </h5>
-            </div>
-          </div>
-          {/* ends */}
-          <div className="row" data-aos="zoom-in-up" data-aos-delay="300">
-            <div className="col xl1 l1 s12 m12">
-              <i
-                className="material-icons Large deep-purple-text darken-4"
-                style={{ fontSize: 40 }}
-              >
-                work
-              </i>
-            </div>
-            <div className="col xl9 l9 s12 m12" style={{ marginTop: -30 }}>
-              <h5
-                style={{
-                  fontSize: 20,
-                  lineHeight: 3.6,
-                  fontFamily: ["Poppins", "sans-serif"],
-                }}
-              >
-                Amazon Application Engineer 2 - 2023 to present
-              </h5>
-            </div>
-          </div>
+        {/* Left Column - Work Experience List */}
+        <div className="col xl6 l6 s12 m12 mt-20">
+          {workExperiences.map((job, index) => (
+            <WorkItem key={index} title={job} delay={300 * (index + 1)} />
+          ))}
         </div>
-        <div className="col xl6 l6 s12 m12 ">
-          <img src={WorkImage} alt={WorkImage} className="responsive-img" />
+
+        {/* Right Column - Image */}
+        <div className="col xl6 l6 s12 m12 flex justify-center items-center">
+          <img src={WorkImage} alt="Work Experience" className="responsive-img" />
         </div>
       </div>
-    </React.Fragment>
+    </section>
   );
 }
 
