2ce08e709116475352f0561171740d138ceafcb3
vignesh K <31525460+vickytechkey@users.noreply.github.com>
2025-09-17 18:05:51 +0530

    uploading day 100 batch

--- src/pages/leetcodebadge/gifcompo.jsx
+++ src/pages/leetcodebadge/gifcompo.jsx
@@ -4,12 +4,14 @@ import top50sql from './asset/Top_SQL_50.gif'
 import day50badge from './asset/day50.gif'
 import advancesql50 from './asset/advancesql50.jpeg'
 import day502025 from './asset/202550days.gif'
+import day1002025 from './asset/2025100days.gif'
 const GifGallery = () => {
   const gifs = [
     { src: day502025, alt: 'Description 1' },
     { src: top50sql, alt: 'Description 1' },
     { src: day50badge, alt: 'Description 1' },
-    { src: advancesql50, alt: 'Description 1' }
+    { src: advancesql50, alt: 'Description 1' },
+    { src: day1002025, alt: 'Description 1' }
     
     // Add more GIFs as needed
   ];
